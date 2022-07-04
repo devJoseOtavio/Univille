3c5678e60f2c684b981e2a44f315f6dd9def3141
otaviojosetrabalho@gmail.com <jose.gallo@asaas.com.br>
2022-07-04 17:40:50 -0300

    Trabalho page, refatorando fb e utilizando js para manipulação de URL

---
+++ facebook/face.js
@@ -0,0 +1,15 @@
+criaConta = document.getElementById("criarConta")
+
+criaConta.addEventListener("click", createAccount);
+
+function createAccount() {
+    window.location.href = "file:///C:/Users/Z%C3%A9/Desktop/Programa%C3%A7%C3%A3o/2022comehere/Univille/facebook/criarconta.html"
+}
+
+Pesquisar = document.querySelector(".Pesquisar");
+
+Pesquisar.addEventListener("click", redirectCreate);
+
+function redirectCreate() {
+    window.location.href = "file:///C:/Users/Z%C3%A9/Desktop/Programa%C3%A7%C3%A3o/2022comehere/Univille/facebook/face.html"
+}
